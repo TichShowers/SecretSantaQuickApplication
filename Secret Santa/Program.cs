a21ba8997b3e8b2af5c5df1c2f1adedb34f48804
Tich Showers <tichsc2@gmail.com>
2015-11-14 16:55:38 +0100

    Only commit you will need

---
+++ Secret Santa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secret_Santa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Person>()
+            {
+                new Person() { Name = "Tich Showers" },
+                new Person() { Name = "Adria Storm" },
+                new Person() { Name = "Rayne Flash" },
+                new Person() { Name = "Mist Showers" },
+                new Person() { Name = "Altich Showers" },
+            };
+
+            list.ShuffleAssignees();
+
+            foreach (var person in list)
+            {
+                Console.WriteLine("{0} was assigned {1}", person.Name, person.Assigned.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Secret Santa/SecretSantaLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secret_Santa
+{
+    public static class SecretSantaLogic
+    {
+        public static void ShuffleAssignees(this IList<Person> target)
+        {
+            var copy = new List<Person>(target);
+
+            var firstPerson = copy.GetRandomElement();
+            copy.Remove(firstPerson);
+
+            var assignee = copy.GetRandomElement();
+            firstPerson.Assigned = assignee;
+
+            while (copy.Count() > 1)
+            {
+                copy.Remove(assignee);
+                var newAssignee = copy.GetRandomElement();
+
+                assignee.Assigned = newAssignee;
+                assignee = newAssignee;
+            }
+
+            assignee.Assigned = firstPerson;
+        }
+
+        private static T GetRandomElement<T>(this IList<T> target)
+        {
+            var random = new Random();
+
+            return target.ElementAt(random.Next(0, target.Count()));
+        }
+    }
+}
